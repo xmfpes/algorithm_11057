ddf0f021ec6b77af5d9b2c25ea1655e889e98ea4
kyu <kyu@172.30.1.1>
2017-08-12 21:57:40 +0900

    11057번 문제 해결

--- src/prob11057/Main.java
+++ src/prob11057/Main.java
@@ -1,7 +1,33 @@
 package prob11057;
 
+import java.util.Scanner;
+
 public class Main {
+	static int mod = 10007;
+	public static int d[][];
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		d = new int[input + 1][10];
+		solve(input);
+	}
+	public static void solve(int n) {
+		for(int i=0; i<=9; i++) {
+			d[1][i] = 1;
+		}
+
+		for(int i=2; i<=n; i++) {
+			for(int j=0; j<=9; j++) {
+				for(int k=j; k<=9; k++) {
+					d[i][j] += d[i-1][k];
+				}
+			}
+		}
+		int solve = 0;
+		for(int i=0; i<=9; i++) {
+			solve += d[n][i];
+		}
+		solve %= mod;
+		System.out.println(solve);
 	}
 }
